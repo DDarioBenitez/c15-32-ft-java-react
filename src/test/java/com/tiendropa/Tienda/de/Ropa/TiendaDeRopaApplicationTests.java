dc1d96c628aa7e6d35ab10fc124e919eb8b8aabb
DDarioBenitez <reedq2.alt@gmail.com>
2023-11-29 18:19:44 -0300

    First Commit

---
+++ src/test/java/com/tiendropa/Tienda/de/Ropa/TiendaDeRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendropa.Tienda.de.Ropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDeRopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
