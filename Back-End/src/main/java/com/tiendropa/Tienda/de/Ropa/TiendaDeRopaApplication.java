b873032603d4f7167dcf2ecf7b92ac1c39df821c
DDarioBenitez <reedq2.alt@gmail.com>
2023-11-30 17:23:43 -0300

    xs

---
+++ Back-End/src/main/java/com/tiendropa/Tienda/de/Ropa/TiendaDeRopaApplication.java
@@ -0,0 +1,13 @@
+package com.tiendropa.Tienda.de.Ropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDeRopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDeRopaApplication.class, args);
+	}
+
+}
