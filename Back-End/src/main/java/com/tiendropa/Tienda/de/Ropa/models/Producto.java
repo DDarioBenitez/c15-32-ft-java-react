6c28d75f51ec4949a75a8648700ddb934c7dfdd0
Enzo Sierra <enzogsierra@gmail.com>
2023-12-01 22:15:56 -0300

    Models refactorizados

--- Back-End/src/main/java/com/tiendropa/Tienda/de/Ropa/models/Producto.java
+++ Back-End/src/main/java/com/tiendropa/Tienda/de/Ropa/models/Producto.java
@@ -1,63 +1,53 @@
 package com.tiendropa.Tienda.de.Ropa.models;
 
-import com.tiendropa.Tienda.de.Ropa.enums.Categoria;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
+import com.tiendropa.Tienda.de.Ropa.enums.Categoria;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
 @Entity
+@Table(name = "productos")
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
 public class Producto {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native")
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Setter@Getter
     private String nombre;
 
-    @Setter@Getter
     private String descripcion;
 
-    @Setter@Getter
-    private double precio;
+    private BigDecimal precio;
 
-    @Setter@Getter
     private String marca;
 
-    @Setter@Getter
     private Categoria categoria;
 
-    @Setter@Getter
     private String talle;
 
-    @Setter@Getter
     private String color;
 
-    @Getter
     @OneToMany(mappedBy = "producto", fetch = FetchType.EAGER)
-    private List<Comentario> comentarios= new ArrayList<>();
+    private List<Comentario> comentarios = new ArrayList<>();
 
-    public Producto(String nombre, String descripcion, double precio, String marca, Categoria categoria, String talle, String color){
-        this.nombre=nombre;
-        this.descripcion=descripcion;
-        this.precio=precio;
-        this.marca=marca;
-        this.categoria= categoria;
-        this.talle= talle;
-        this.color= color;
-    }
-    public long getId(){
-        return this.id;
-    }
 
-    public void addCometario(Comentario comentario){
+    public void addCometario(Comentario comentario) {
         comentario.setProducto(this);
         this.comentarios.add(comentario);
     }
