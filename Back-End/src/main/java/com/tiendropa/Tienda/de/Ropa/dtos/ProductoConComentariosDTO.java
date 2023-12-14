c8d13bb6bd1b8322430d34a47da6a8e2519f74a4
DDarioBenitez <reedq2.alt@gmail.com>
2023-12-14 16:23:48 -0300

    cors

--- Back-End/src/main/java/com/tiendropa/Tienda/de/Ropa/dtos/ProductoConComentariosDTO.java
+++ Back-End/src/main/java/com/tiendropa/Tienda/de/Ropa/dtos/ProductoConComentariosDTO.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @NoArgsConstructor@AllArgsConstructor@Getter
