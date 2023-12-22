f1b0cf1beaa0202cd59c8dfe045da12112b57ed0
DDarioBenitez <reedq2.alt@gmail.com>
2023-12-22 16:38:00 -0300

    cositas

--- Front-End/src/js/services/AxiosService.js
+++ Front-End/src/js/services/AxiosService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080/api";
+axios.defaults.baseURL = "https://c15-32-ft-java-react-production.up.railway.app/api";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 
 export const request = (method, url, data = {}) => {
-    return axios({method: method, url: url, data: data});
+    return axios({ method: method, url: url, data: data });
 }
